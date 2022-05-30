0949e785b22aaa26893e4eaa63fc35f63e257244
onvie <onvie@ASUSVIVOBOOKA51>
2022-05-30 16:14:36 +0700

    Add project files.

---
+++ LabBigSchool-OnVietHung/Models/CoursesController.cs
@@ -0,0 +1,50 @@
+﻿using LabBigSchool_OnVietHung.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabBigSchool_OnVietHung.Models
+{
+
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        // GET: Courses
+        public CoursesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _context.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _context.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _context.Course.Add(course);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ LabBigSchool-OnVietHung/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_OnVietHung.Startup))]
+namespace LabBigSchool_OnVietHung
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
